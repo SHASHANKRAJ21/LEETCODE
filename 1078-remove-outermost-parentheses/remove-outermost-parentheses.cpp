2d1b361305878dd61674ba0463917a687e49502b
SHASHANK RAJ GUPTA <126189181+SHASHANKRAJ21@users.noreply.github.com>
2024-09-14 14:20:14 +0530

    Time: 5 ms (48.03%) | Memory: 8.2 MB (75.40%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string result;
-        int open = 0; // To track the number of open parentheses
-
-        for (char c : s) {
-            if (c == '(') {
-                if (open > 0) {
-                    result += c; // Add '(' only if it's not the outermost
+        string result="";
+        int open=0;
+        for(char c : s){
+            if(c=='('){
+                if(open>0){
+                    result+="(";
                 }
-                open++; // Increment open parentheses count
-            } else {
-                open--; // Decrement open parentheses count
-                if (open > 0) {
-                    result += c; // Add ')' only if it's not the outermost
+                open++;
+            }
+            else{
+                open--;
+                if(open>0){
+                    result+=")";
                 }
             }
-        }
 
+        }
         return result;
     }
 };
