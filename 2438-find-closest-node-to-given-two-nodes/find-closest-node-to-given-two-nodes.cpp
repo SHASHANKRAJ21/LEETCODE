eb0b38792f54ac97ee7fa820fa4d9ba7f710d8a7
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-04-15 20:52:45 +0530

    Time: 16 ms (65.37%) | Memory: 101.8 MB (93.98%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
+    void getDistances(vector<int>& edges, int start, vector<int>& dist) {
+        int d = 0;
+        while (start != -1 && dist[start] == -1) {
+            dist[start] = d++;
+            start = edges[start];
+        }
+    }
+
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        int n=edges.size();
-        vector<int>dist1(n,-1);
-        vector<int>dist2(n,-1);
-        int d=1;
-        while(node1!=-1){
-            if(dist1[node1]!=-1){
-                break;
-            }
-            dist1[node1]+=d;
-            node1=edges[node1];
-            d++;
-        }
-        d=1;
-            while(node2!=-1){
-            if(dist2[node2]!=-1){
-                break;
-            }
-            dist2[node2]+=d;
-            node2=edges[node2];
-            d++;
-        }
-        int mind=INT_MAX;
-        int ans=-1;
-        for(int i=0;i<n;i++){
-            if(dist1[i]!=-1&&dist2[i]!=-1){
-                if(mind>max(dist1[i],dist2[i])){
-                    mind=max(dist1[i],dist2[i]);
-                    ans=i;
+        int n = edges.size();
+        vector<int> dist1(n, -1), dist2(n, -1);
+
+        getDistances(edges, node1, dist1);
+        getDistances(edges, node2, dist2);
+
+        int minDist = INT_MAX;
+        int result = -1;
+
+        for (int i = 0; i < n; ++i) {
+            if (dist1[i] != -1 && dist2[i] != -1) {
+                int maxDist = max(dist1[i], dist2[i]);
+                if (maxDist < minDist) {
+                    minDist = maxDist;
+                    result = i;
+                } else if (maxDist == minDist && i < result) {
+                    result = i;
                 }
             }
         }
-        return ans;
 
+        return result;
     }
 };
