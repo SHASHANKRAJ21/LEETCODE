8bc19c431bfd636e9ca69334ffb1d1cf9a9fef8e
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-04-15 22:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (53.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-    const int m = matrix.size();
-    const int n = matrix[0].size();
-    bool shouldFillFirstRow = false;
-    bool shouldFillFirstCol = false;
+   void setZeroes(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    if (m == 0) return; 
+    int n = matrix[0].size();
+    if (n == 0) return;
 
-    for (int j = 0; j < n; ++j)
-      if (matrix[0][j] == 0) {
-        shouldFillFirstRow = true;
-        break;
-      }
+    bool firstRowZero = false, firstColZero = false;
 
     for (int i = 0; i < m; ++i)
-      if (matrix[i][0] == 0) {
-        shouldFillFirstCol = true;
-        break;
-      }
+        if (matrix[i][0] == 0)
+            firstColZero = true;
 
-    // Store the information in the first row and the first column.
+    for (int j = 0; j < n; ++j)
+        if (matrix[0][j] == 0)
+            firstRowZero = true;
+
     for (int i = 1; i < m; ++i)
-      for (int j = 1; j < n; ++j)
-        if (matrix[i][j] == 0) {
-          matrix[i][0] = 0;
-          matrix[0][j] = 0;
-        }
+        for (int j = 1; j < n; ++j)
+            if (matrix[i][j] == 0)
+                matrix[i][0] = matrix[0][j] = 0;
 
-    // Fill 0s for the matrix except the first row and the first column.
     for (int i = 1; i < m; ++i)
-      for (int j = 1; j < n; ++j)
-        if (matrix[i][0] == 0 || matrix[0][j] == 0)
-          matrix[i][j] = 0;
+        for (int j = 1; j < n; ++j)
+            if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                matrix[i][j] = 0;
 
-    // Fill 0s for the first row if needed.
-    if (shouldFillFirstRow)
-      for (int j = 0; j < n; ++j)
-        matrix[0][j] = 0;
+    if (firstRowZero)
+        for (int j = 0; j < n; ++j)
+            matrix[0][j] = 0;
 
-    // Fill 0s for the first column if needed.
-    if (shouldFillFirstCol)
-      for (int i = 0; i < m; ++i)
-        matrix[i][0] = 0;
-  }
+    if (firstColZero)
+        for (int i = 0; i < m; ++i)
+            matrix[i][0] = 0;
+}
+
 };
