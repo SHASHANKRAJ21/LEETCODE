6e20421f6636f3182749b966a30edd0fcc8c8b61
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-04-23 00:43:31 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (57.96%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void reverse(vector<int>&nums,int start,int end){
-        while(start<end){
+        while(start<=end){
             int temp=nums[start];
             nums[start]=nums[end];
             nums[end]=temp;
