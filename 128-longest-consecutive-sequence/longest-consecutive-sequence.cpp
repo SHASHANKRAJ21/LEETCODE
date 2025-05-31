112c356bbb5b4eb0b984844b16d54366d460a814
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-05-31 12:04:28 +0530

    Time: 83 ms (60.14%) | Memory: 88.8 MB (50.55%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -6,22 +6,22 @@ public:
             return 0;
         }
         int longest=1;
-        unordered_set<int> st;
+        unordered_set<int>st;
         for(int i=0;i<n;i++){
             st.insert(nums[i]);
         }
-        for(auto it : st){
+        for(auto it:st){
             if(st.find(it-1)==st.end()){
                 int cnt=1;
                 int x=it;
                 while(st.find(x+1)!=st.end()){
                     x=x+1;
                     cnt=cnt+1;
+
                 }
                 longest=max(longest,cnt);
             }
         }
-
-    return longest;    
+        return longest;    
     }
 };
