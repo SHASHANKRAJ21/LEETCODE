a3eba4090d339840920ff0a99bf87b71627e700f
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-06-14 13:04:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (34.74%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,25 +1,29 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
 class Solution {
 public:
     bool isValid(string s) {
-    stack<char> stack;
+        stack<char> st;
 
-    for (const char c : s)
-      if (c == '(')
-        stack.push(')');
-      else if (c == '{')
-        stack.push('}');
-      else if (c == '[')
-        stack.push(']');
-      else if (stack.empty() || pop(stack) != c)
-        return false;
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.empty()) return false;
 
-    return stack.empty();
-  }
+                char top = st.top();
+                st.pop();
 
- private:
-  int pop(stack<char>& stack) {
-    const int c = stack.top();
-    stack.pop();
-    return c;
-  }
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
 };
