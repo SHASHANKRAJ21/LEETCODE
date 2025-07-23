2ae390f361331fe186e85f038954d0b545fce0f0
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-07-23 19:37:59 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (50.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -4,12 +4,12 @@ public:
         int n=nums.size();
         int i=0;
         for(int j=0;j<n;j++){
-            if(nums[i]!=nums[j]){
+            if(nums[j]!=nums[i]){
                 nums[i+1]=nums[j];
                 i++;
             }
+            
         }
         return i+1;
-        
     }
 };
