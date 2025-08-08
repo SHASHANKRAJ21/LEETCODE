5f54fcbc2423a7b2ba5ae65fa46902d5ca226a63
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-08-08 17:30:34 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (65.97%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,29 +4,27 @@ public:
         int n=nums.size();
         int low=0;
         int high=n-1;
-        //basically removing a sorted left and sorted right
         while(low<=high){
-           int mid=(low+high)/2;
-           if(nums[mid]==target){
-            return mid;
-           }
-           if(nums[low]<=nums[mid]){
-            if(nums[low]<=target && target<=nums[mid]){
-                high=mid-1;
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
             else{
-                low=mid+1;
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
-
-           }
-           else{
-            if(nums[mid]<=target && target<=nums[high]){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-           }
         }
         return -1;
     }
