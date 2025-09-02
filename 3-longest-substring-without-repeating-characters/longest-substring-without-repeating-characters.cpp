84039c3c5155c666e70917439dc2a0c25f05a380
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-09-02 18:29:20 +0530

    Time: 15 ms (39.83%) | Memory: 14.4 MB (22.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int>hash(256,-1);
+        unordered_set<char>st;
         int left=0;
-        int right=0;
-        int len=0;
-        int n=s.size();
-        while(right<n){
-            if(hash[s[right]]!=-1){
-                left=max(hash[s[right]]+1,left);
+        int maxlen=0;
+        for(int right=0;right<s.size();right++){
+            while(st.find(s[right])!=st.end()){
+                st.erase(s[left]);
+                left++;
             }
-            hash[s[right]]=right;
-            len=max(right-left+1,len);
-            right++;
-        }
-        return len;
+            st.insert(s[right]);
+            maxlen=max(maxlen,right-left+1);
 
-        
+        }
+        return maxlen;
     }
 };
