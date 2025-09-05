0d37623e93f08a1e66a99eb1272f3b7d61a13972
SHASHANK RAJ GUPTA <shashank286raj@gmail.com>
2025-09-05 16:00:20 +0530

    Time: 11 ms (69.74%) | Memory: 9.4 MB (81.69%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,34 +1,35 @@
-
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n < 2) return s;
+        if(s.empty()){
+            return " ";
+        } 
+        int start=0;
+        int maxlen=0;
+        for(int i=0;i<s.size();i++){
+            int l=i;
+            int r=i;
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if(r-l+1>maxlen){
+                    start=l;
+                    maxlen=r-l+1;
+                }
+                l--;
+                r++;
 
-        int bestL = 0, bestR = 0;
-
-        auto expand = [&](int l, int r) {
-            while (l >= 0 && r < n && s[l] == s[r]) {
-                l--; r++;
             }
-            // return inclusive bounds after stepping back
-            return pair<int,int>{l + 1, r - 1};
-        };
-
-        for (int i = 0; i < n; ++i) {
-            // odd length
-            auto [l1, r1] = expand(i, i);
-            if (r1 - l1 > bestR - bestL) {
-                bestL = l1; bestR = r1;
-            }
-
-            // even length
-            auto [l2, r2] = expand(i, i + 1);
-            if (r2 - l2 > bestR - bestL) {
-                bestL = l2; bestR = r2;
+            l=i;
+            r=i+1;
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if(r-l+1>maxlen){
+                    start=l;
+                    maxlen=r-l+1;
+                }
+                l--;
+                r++;
             }
         }
-        return s.substr(bestL, bestR - bestL + 1);
+        return s.substr(start,maxlen);
+
     }
 };
-
